09505c4cd5ca3afe05713747fb41b9f5cdc7af8c
Noah Hüsser <noah@huesser.dev>
2025-01-08 02:17:35 +0100

    Fix hotplug waker

--- src/platform/webusb/hotplug.rs
+++ src/platform/webusb/hotplug.rs
@@ -1,5 +1,8 @@
 use std::{
-    sync::mpsc::{channel, Receiver, TryRecvError},
+    sync::{
+        mpsc::{channel, Receiver, TryRecvError},
+        Arc,
+    },
     task::Poll,
 };
 
@@ -15,7 +18,7 @@ use crate::{hotplug::HotplugEvent, Error};
 use super::{enumeration::device_to_info, DeviceId};
 
 pub(crate) struct WebusbHotplugWatch {
-    waker: AtomicWaker,
+    waker: Arc<AtomicWaker>,
     events: Receiver<HotplugEvent>,
 }
 
@@ -24,57 +27,62 @@ impl WebusbHotplugWatch {
         let window = web_sys::window().unwrap();
         let navigator = window.navigator();
         let usb = navigator.usb();
+        let waker = Arc::new(AtomicWaker::new());
         let (sender, receiver) = channel();
         {
             let sender = sender.clone();
-            usb.set_onconnect(Some(
+            let waker = waker.clone();
+            let onconnect = Closure::wrap(Box::new(move |event: UsbConnectionEvent| {
+                let sender = sender.clone();
+                let waker = waker.clone();
+                spawn_local(async move {
+                    let info = device_to_info(event.device()).await;
+                    match info {
+                        Ok(info) => {
+                            let result = sender.clone().send(HotplugEvent::Connected(info));
+                            if let Err(e) = result {
+                                tracing::warn!(
+                                    "Could not send the connect event to the internal channel: {e:?}",
+                                )
+                            } else {
+                                waker.wake();
+                            }
+                        }
+                        Err(e) => {
+                            tracing::warn!(
+                                 "Could not read device descriptors for internal connect event dispatch: {e:?}",
+                             )
+                        }
+                    }
+                })
+            }) as Box<dyn FnMut(UsbConnectionEvent)>);
+            usb.set_onconnect(Some((onconnect).as_ref().unchecked_ref()));
+        }
+        {
+            let sender = sender.clone();
+            let waker = waker.clone();
+            usb.set_ondisconnect(Some(
                 (Closure::wrap(Box::new(move |event: UsbConnectionEvent| {
                     let sender = sender.clone();
-                    spawn_local(async move {
-                        let info =  device_to_info(event.device()).await;
-                        match info {
-                            Ok(info) => {let result = sender.clone().send(HotplugEvent::Connected(
-                               info ,
-                             ));
-                             if let Err(e) = result {
-                                 tracing::warn!(
-                                     "Could not send the connect event to the internal channel: {e:?}",
-                                 )
-                             }},
-                            Err(e) => {
-                                 tracing::warn!(
-                                     "Could not read device descriptors for internal connect event dispatch: {e:?}",
-                                 )
-                             },
-                        }
-                        
-                    })
+                    let waker = waker.clone();
+                    let result = sender.send(HotplugEvent::Disconnected(crate::DeviceId(
+                        DeviceId::from_device(&event.device()),
+                    )));
+                    if let Err(e) = result {
+                        tracing::warn!(
+                            "Could not send the disconnect event to the internal channel: {e:?}",
+                        )
+                    } else {
+                        waker.wake();
+                    }
                 }) as Box<dyn FnMut(UsbConnectionEvent)>))
                 .as_ref()
                 .unchecked_ref(),
             ));
         }
-        {
-            let sender = sender.clone();
-            usb.set_ondisconnect(Some(
-                    (Closure::wrap(Box::new(move |event: UsbConnectionEvent| {
-                        let sender = sender.clone();
-                        let result = sender.send(HotplugEvent::Disconnected(
-                            crate::DeviceId(DeviceId::from_device(&event.device()))
-                        ));
-                        if let Err(e) = result {
-                            tracing::warn!(
-                                "Could not send the disconnect event to the internal channel: {e:?}",
-                            )
-                        }
-                    }) as Box<dyn FnMut(UsbConnectionEvent)>))
-                    .as_ref()
-                    .unchecked_ref(),
-                ));
-        }
         Ok(Self {
-            waker: AtomicWaker::new(),
-            events:  receiver,
+            waker,
+            events: receiver,
         })
     }
 
